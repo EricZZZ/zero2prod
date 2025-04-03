cda06ea9e37103af172a469c915a6cc76a8b5239
EricZzz <biggogten@gmail.com>
2025-04-03 20:50:17 +0800

    feat:add subscritions api and tests

--- src/startup.rs
+++ src/startup.rs
@@ -2,15 +2,21 @@ use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // 使用智能指针包裹 connection
+    let db_pool = web::Data::new(db_pool);
+    let local_addr = &listener.local_addr().unwrap();
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
+    println!("Listening on {}", local_addr.port());
     Ok(server)
 }
